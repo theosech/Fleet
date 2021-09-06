3c1df6bad17f0547014225b4c45ff82b28a61482
Theo <theosech@mit.edu>
2021-09-06 18:32:43 -0400

    initial propsim changes; added flag and compute_likelihood method

---
+++ Models/JoshRule/model/run.sh
@@ -0,0 +1,22 @@
+
+myhost=$(hostname -s)
+d=2021-05-13-15-05-01_model_comparison_data
+f=c001_2.txt
+
+# ls $d/*.txt | xargs -n 1 basename | parallel --jobs=48 "./main --largeAlphabet=1 --chains=5 --threads=1 --time=10m --input=$d/{1} --output=out-large/{1} > out-large/{1}.log" 
+
+# ls $d/*.txt | xargs -n 1 basename | parallel --jobs=48 "./main --largeAlphabet=0 --chains=5 --threads=1 --time=10m --input=$d/{1} --output=out-small/{1} > out-small/{1}.log" 
+ 
+# test command for single task
+./main --largeAlphabet=0 --chains=5 --threads=1 --time=1ms --input=$d/$f --output=out-small/$f > out-small/$f.log
+# ./main --largeAlphabet=0 --chains=5 --threads=1 --time=1ms --input=2021-05-13-15-05-01_model_comparison_data/c001_2.txt --output=out-small/c001_2.txt > out-small/c001_2.txt.log
+
+## The below is for running A*
+# 
+# {
+# ls txt-smallAlphabet/*.txt | xargs -n 1 basename | parallel --jobs=2 "./main --doAstar --chains=5 --threads=1 --time=10m --input=txt-smallAlphabet/{1} --output=out-astar/{1} > out-astar/{1}.log" 
+# } &
+# 
+# {
+# ls txt-largeAlphabet/*.txt | xargs -n 1 basename | parallel --jobs=2 "./main --doAstar --largeAlphabet=1 --chains=5 --threads=1 --time=10m --input=txt-largeAlphabet/{1} --output=out-astar/{1} > out-astar/{1}.log" 
+# } &
